25fe70c01853f0be75a5b340ef05d64154cc3aa9
Kylie Rose <thisiskylierose@gmail.com>
2014-12-02 10:25:16 +1000

    added polyfill for IE8

--- src/assets/script/app.beta.js
+++ src/assets/script/app.beta.js
@@ -8,6 +8,14 @@ if (!String.prototype.contains) {
     };
 }
 
+if (!Object.keys) Object.keys = function(o) {
+    if (o !== Object(o))
+        throw new TypeError('Object.keys called on a non-object');
+    var k=[],p;
+    for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+    return k;
+}
+
 qg.swe.emergency = (function ($, swe, Date, Mocks) {
     'use strict';
 
