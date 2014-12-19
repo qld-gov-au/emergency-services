ceb9a1b7cedf9b6470336ef1726b35bf37879e87
Kylie Rose <thisiskylierose@gmail.com>
2014-12-19 11:43:41 +1000

    removed some tests

--- test/e2e/test_spec.js
+++ test/e2e/test_spec.js
@@ -7,30 +7,30 @@ describe('test interface', function () {
     });
 
     it('check the title', function () {
-        var title = element(by.css('.search-widget .widget-header h2'));
-        expect(title.getText()).toBe('Search for services');
+        var title = element(by.css('#content h1'));
+        expect(title.getText()).toBe('Emergency services and safety');
     });
 
-    it('check the toggle', function () {
-        var form = element(by.css('#services-form'));
-        var toggle = element(by.css('#services-toggle'));
-        // on click
-        toggle.click();
-        expect(form.isDisplayed()).toBe(false);
-        // on second click
-        toggle.click();
-        expect(form.isDisplayed()).toBe(true);
-    });
-
-    // this is a slightly different version
-    it('check the toggle (again)', function () {
-        var form = element(by.css('#services-form'));
-        var toggle = element(by.css('#services-toggle'));
-        // on click
-        browser.actions().click(toggle).perform();
-        expect(form.isDisplayed()).toBe(false);
-        // on second click
-        browser.actions().click(toggle).perform();
-        expect(form.isDisplayed()).toBe(true);
-    });
+//    it('check the toggle', function () {
+//        var form = element(by.css('#services-form'));
+//        var toggle = element(by.css('#services-toggle'));
+//        // on click
+//        toggle.click();
+//        expect(form.isDisplayed()).toBe(false);
+//        // on second click
+//        toggle.click();
+//        expect(form.isDisplayed()).toBe(true);
+//    });
+//
+//    // this is a slightly different version
+//    it('check the toggle (again)', function () {
+//        var form = element(by.css('#services-form'));
+//        var toggle = element(by.css('#services-toggle'));
+//        // on click
+//        browser.actions().click(toggle).perform();
+//        expect(form.isDisplayed()).toBe(false);
+//        // on second click
+//        browser.actions().click(toggle).perform();
+//        expect(form.isDisplayed()).toBe(true);
+//    });
 });
